ef5c36579b59b58566c90327c99a7ddf5cad1ef7
cancio <filipe.cancio@gmail.com>
2022-04-23 18:52:21 -0300

    Create a new android app

---
+++ bottom/app/src/main/java/dev/cancio/bottom/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.cancio.bottom
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
