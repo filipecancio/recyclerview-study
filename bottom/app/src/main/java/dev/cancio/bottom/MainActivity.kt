3a1ea4b7841c8d178d883696e730bcf2f0662b10
cancio <filipe.cancio@gmail.com>
2022-04-23 19:08:09 -0300

    setting viewbinding

--- bottom/app/src/main/java/dev/cancio/bottom/MainActivity.kt
+++ bottom/app/src/main/java/dev/cancio/bottom/MainActivity.kt
@@ -2,10 +2,14 @@ package dev.cancio.bottom
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dev.cancio.bottom.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
